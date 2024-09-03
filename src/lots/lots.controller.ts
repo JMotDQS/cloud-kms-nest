139741554906add89f0d4232b814cc679b45f960
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-09-03 14:05:35 -0400

    added lots CRUD controllers

--- src/lots/lots.controller.ts
+++ src/lots/lots.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('lots')
-export class LotsController {}
+@Controller('lots') // /lots
+export class LotsController {
+	/*
+		GET /lots
+		GET /lots/:id
+		POST /lots
+		PUT/PATCH /lots/:id
+		DELETE /lots/:id
+	*/
+
+	@Get() // GET /lots or /lots?active=value&lot_name=value
+	getLots(@Query('active') active?: '1' | '0') {
+		return [];
+	}
+
+	@Get(':id') // GET /lots/:id
+	getLot(@Param('id') id: string) {
+		return {id};
+	}
+
+	@Post() // POST /lots
+	create(@Body() lot: {}) {
+		return lot;
+	}
+
+	@Patch(':id') // PUT/PATCH /lots/:id
+	updateLot(@Param('id') id: string, @Body() lotUpdate: {}) {
+		return { id, ...lotUpdate };
+	}
+
+	@Delete(':id') // DELETE /lots/:id
+	deleteLot(@Param('id') id: string) {
+		return { id };
+	}
+}
--- src/lots/lots.module.ts
+++ src/lots/lots.module.ts
@@ -3,7 +3,7 @@ import { LotsController } from './lots.controller';
 import { LotsService } from './lots.service';
 
 @Module({
-    controllers: [LotsController],
-    providers: [LotsService]
+	controllers: [LotsController],
+	providers: [LotsService]
 })
 export class LotsModule {}
