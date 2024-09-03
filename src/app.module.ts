065902ca3f744809b483667cf01fb01f95ad1c3d
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-09-03 12:46:20 -0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { LotsModule } from './lots/lots.module';
 
 @Module({
-	imports: [UsersModule, LotsModule],
+	imports: [UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
