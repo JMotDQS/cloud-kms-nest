219b535303132fda8c338ba4f9be93d6423bca51
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-09-27 10:40:02 -0400

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,22 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('users')
+@Controller('users')// handles routing for anything going to the /users route(directory)
 export class UsersController {
 	/*
-		GET /users
-		GET /users/:id
-		POST /users
-		PUT/PATCH /users/:id
-		DELETE /users/:id
+		GET /users - gets all users
+		GET /users/:id - gets specific user
+		POST /users - adds a new user
+		PUT/PATCH /users/:id - updates a specific user
+		DELETE /users/:id - deletes a specific user
 	*/
 
-	@Get() // GET /users
+	/*@Get() // GET /users
 	findAll() {
+		return [];
+	}*/
+
+	@Get() // GET /users or /users?role=value
+	findAll(@Query('role') role?: 'SECURITY SUPERVISOR' | 'SECURITY GUARD' | 'DRIVEAWAY' | 'ADMIN') {
 		return [];
 	}
 
