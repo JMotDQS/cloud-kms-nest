677a48180864f39c1065ba3a895006fc6c57428c
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-11-20 16:57:01 -0500

    Trying Sesqulize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { LotsModule } from './lots/lots.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
 	imports: [
+		SequelizeModule.forRoot({
+			dialect: 'mysql',
+			host: 'localhost',
+			port: 3396,
+			username: 'root',
+			password: 'L0rdOfL1ghtn!ng',
+			database: 'kms_v2_mysql',
+			models: [],
+		}),
 		/*TypeOrmModule.forRoot({
 			type: 'mssql',
 			host: 'localhost',
--- src/lots/lots.module.ts
+++ src/lots/lots.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
 import { LotsController } from './lots.controller';
 import { LotsService } from './lots.service';
+import { Column, CreatedAt, Model, PrimaryKey, Table } from 'sequelize-typescript';
 
 @Module({
 	controllers: [LotsController],
 	providers: [LotsService]
 })
-export class LotsModule {}
+
+@Table
+export class LotsModule extends Model {
+	@PrimaryKey
+	declare pk_id: string;
+
+	@CreatedAt
+	created_date: Date;
+
+	@Column
+	lot_name: string;
+
+	@Column
+	lot_address: string;
+
+	@Column
+	lot_city: string;
+
+	@Column
+	fk_g_states_pk_id: string;
+
+	@Column
+	lot_zip: string;
+
+	@Column
+	lot_capacity: number;
+
+	@Column
+	lot_active: number;
+
+	@Column
+	fk_manufacturers_pk_id: string;
+}
