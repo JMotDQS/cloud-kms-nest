a29ab708ec1f3a2b5fde2fdc311cec27f123b947
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-12-02 13:14:27 -0500

    Update database.providers.ts

--- src/database.providers.ts
+++ src/database.providers.ts
@@ -6,14 +6,15 @@ export const databaseProviders = [
 	{
 		provide: 'SEQUELIZE',
 		useFactory: async () => {
-		const sequelize = new Sequelize({
-			dialect: 'mysql',
-			host: 'localhost',
-			port: 3369,
-			username: 'root',
-			password: 'L0rdOfL1ghtn!ng',
-			database: 'kms_v2_mysql',
-		});
+			const sequelize = new Sequelize({
+				dialect: 'mysql',
+				host: 'localhost',
+				port: 3369,
+				username: 'root',
+				password: 'L0rdOfL1ghtn!ng',
+				database: 'kms_v2_mysql',
+			}
+		);
 		sequelize.addModels([Lot]);
 		await sequelize.sync();
 		return sequelize;
