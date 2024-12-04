53c1e230ae60cc3a7cd4a7ffe395881998f354d4
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-12-04 13:08:11 -0500

    clean up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LotsModule } from './lots/lots.module';
 
 @Module({
-	imports: [LotsModule],
+	imports: [],
 	controllers: [AppController],
 	providers: [AppService],
 })
