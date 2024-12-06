741c9c8f16ecb36185f5e9aa88f31f7f811c72b6
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-12-06 14:56:32 -0500

    clean up

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -66,37 +66,3 @@ export class CreateUserDto {
 	@IsNumber()
 	is_admin: number;
 }
-
-/*export class CreateUserDto {
-	@IsNotEmpty({
-		message: 'Firstname is required'
-	})
-	@IsString()
-	@MinLength(2)
-	@MaxLength(30)
-	firstname: string;
-
-	@IsNotEmpty({
-		message: 'Lastname is required'
-	})
-	@IsString()
-	@MinLength(2)
-	@MaxLength(30)
-	lastname: string;
-
-	@IsNotEmpty({
-		message: 'Email is required'
-	})
-	@IsEmail()
-	email: string;
-
-	@IsNotEmpty({
-		message: 'Password is required'
-	})
-	@MinLength(6)
-	@MaxLength(20)
-	@Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-		message: 'Password is too weak',
-	})
-	password: string;
-}*/
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
+import { UsersService } from "./users.service";
+import { UsersController } from './users.controller';
 import { Models } from "src/models.db";
 
 @Module({
